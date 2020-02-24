c651f15f9938d4fdc63db9351edc257f399af0d7
AlexScigalszky <alexpablo.90@gmail.com>
2020-02-24 17:47:35 -0300

    new way

--- C#/Iterators/QueryableLazyFetch/IQueryableLazyFetch.cs
+++ C#/Iterators/QueryableLazyFetch/IQueryableLazyFetch.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PalabrasAleatoriasOrigenDeDatos.Iterators.QueryableLazyFetch
 {
@@ -12,71 +11,85 @@ namespace PalabrasAleatoriasOrigenDeDatos.Iterators.QueryableLazyFetch
         private IQueryable<T> iQueryable;
         private int currentIndex = -1;
         private int maxIndex = 0;
+        private int currentPageIndex = -1;
         private int pageIndex = 0;
         private IEnumerable<T> currentPage;
 
         public IQueryableLazyFetchEnumerator(IQueryable<T> items)
         {
-            PageSize = 1000;
+            PageSize = 20;
             iQueryable = items;
-            maxIndex = iQueryable.Count() - 1;
+            maxIndex = iQueryable.Count();
+            currentPage = iQueryable.Skip(pageIndex * PageSize).Take(PageSize).ToArray();
+        }
+        public IQueryableLazyFetchEnumerator(IQueryable<T> items, int pageSize): this(items)
+        {
+            PageSize = pageSize;
+        }
+
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            currentPageIndex++;
+            var canMoveNext = currentIndex < maxIndex;
+            if (canMoveNext)
+            {
+                var needLoadNewPage = currentPageIndex == this.PageSize;
+                if (needLoadNewPage)
+                {
+                    currentPageIndex = 0;
+                    pageIndex++;
+                    currentPage = iQueryable.Skip(pageIndex * PageSize).Take(PageSize).ToArray();
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+            currentPageIndex = -1;
+            pageIndex = 0;
             currentPage = iQueryable.Skip(pageIndex * PageSize).Take(PageSize).ToArray();
         }
 
-        public T Current { get; private set; } = default;
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
 
-        object IEnumerator.Current => Current;
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return currentPage.ElementAt(currentPageIndex);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
 
         public void Dispose()
         {
             iQueryable = null;
             currentIndex = -1;
             maxIndex = 0;
-            Current = default;
             currentPage = null;
+            currentPageIndex = -1;
             pageIndex = 0;
-        }
-
-        public bool MoveNext()
-        {
-            var isFirstOne = currentIndex == -1 && this.maxIndex > 0;
-            if (isFirstOne)
-            {
-                currentIndex++;
-                Current = currentPage.ElementAt(currentIndex - pageIndex * PageSize);
-                return true;
-            }
-            var isLastOne = currentIndex + 1 == this.maxIndex;
-            if (isLastOne)
-            {
-                currentIndex++;
-                Current = currentPage.ElementAt(currentIndex - pageIndex * PageSize);
-                return true;
-            }
-            var isInNextPage = currentIndex + 1 == ((pageIndex + 1) * PageSize) && currentIndex < maxIndex;
-            if (isInNextPage)
-            {
-                currentIndex++;
-                pageIndex++;
-                currentPage = iQueryable.Skip(pageIndex * PageSize).Take(PageSize).ToArray();
-                Current = currentPage.ElementAt(currentIndex - pageIndex * PageSize);
-                return true;
-            }
-            var isInCurrentPage = currentIndex + 1 <= ((pageIndex + 1) * PageSize) && currentIndex < maxIndex;
-            if (isInCurrentPage)
-            {
-                currentIndex++;
-                Current = currentPage.ElementAt(currentIndex - pageIndex * PageSize);
-                return true;
-            }
-            return false;
-        }
-
-        public void Reset()
-        {
-            currentIndex = -1;
-            pageIndex = 0;
-            currentPage = iQueryable.Skip(pageIndex * PageSize).Take(PageSize).ToArray();
         }
     }
 

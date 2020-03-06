6072159a54ef2f1a0c35571f9f540eddb3afd727
AlexScigalszky <alexpablo.90@gmail.com>
2020-03-06 18:02:47 -0300

    An Elmah helper for raise exceptions (solve the problem of circular dependeces)

---
+++ C#/ElmahUtils.cs
@@ -0,0 +1,31 @@
+﻿using ElmahCore;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace EDAviation.Shared.Utils
+{
+    public class ElmahUtils
+    {
+        public static string GetJsonFromObject<T>(IQueryable<T> Entities)
+        {
+            return JsonConvert.SerializeObject(Entities.ToArray());
+        }
+
+        public static void ManageExceptionContext(Exception ex)
+        {
+            ElmahExtensions.RiseError(ex);
+        }
+
+        public static MemoryStream GetMemoryStreamFromFile(string WebRootFolder, string FileName)
+        {
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(Path.Combine(WebRootFolder, FileName), FileMode.Open))
+                stream.CopyTo(memory);
+
+            memory.Position = 0;
+            return memory;
+        }
+    }
+}

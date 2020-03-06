a9140fd94fe6e0efaf280e3b26903e6fe857f487
AlexScigalszky <alexpablo.90@gmail.com>
2020-03-06 18:03:23 -0300

    A Helper for API header restriccion

---
+++ C#/HeaderFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using EDAviation.Shared;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EDAviation.API.Filters
+{
+    public class HeaderFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = Constants.USER_TOKEN,
+                Required = true,
+                In = ParameterLocation.Header,
+                Description = "Bearer access token",
+                Schema = new OpenApiSchema { Type = "string" }
+            });
+        }
+    }
+}

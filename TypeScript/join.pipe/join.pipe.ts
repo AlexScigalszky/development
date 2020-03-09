9a854037dc7ada187d3c05f9f692f37a6f57c0df
AlexScigalszky <alexpablo.90@gmail.com>
2020-03-09 13:14:42 -0300

    Join Pipe

---
+++ TypeScript/join.pipe/join.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { isArray } from 'util';
+
+@Pipe({
+  name: 'arrayjoin'
+})
+export class JoinPipe implements PipeTransform {
+
+  transform(input: any, character: string = ', '): any {
+    if (!isArray(input)) {
+      return input;
+    }
+
+    return input.join(character);
+  }
+
+}
---
+++ TypeScript/join.pipe/readme.md
@@ -0,0 +1,3 @@
+# JoinPipe
+
+join multiples values with a character or, by default, with `,` 

3a119a663ac378d4c1b48300f6dd53a399a83c15
AlexScigalszky <alexpablo.90@gmail.com>
2020-11-27 19:04:00 -0300

    converters

---
+++ C#/Converters/Base64/Base64Converter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Converters.Base64
+{
+    public class Base64Converter : IBase64Converter
+    {
+        public byte[] FromBase64String(string s)
+        {
+            return Convert.FromBase64String(s);
+        }
+    }
+}
---
+++ C#/Converters/Base64/Base64ConverterMock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Converters.Base64
+{
+    public class Base64ConverterMock : IBase64Converter
+    {
+        public byte[] FromBase64String(string s)
+        {
+            return new byte[] { };
+        }
+    }
+}

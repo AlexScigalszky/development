ff53fe6cc0e0dc12d4b154ea0943a038f9b8bbc8
AlexScigalszky <alexpablo.90@gmail.com>
2020-11-27 19:07:22 -0300

    getUserId

---
+++ C#/UserIdGetter/UserIdGetter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Services.UserIdGetter
+{
+    public class UserIdGetter : IUserIdGetter
+    {
+        private string CustomUserId = null;
+
+        public string GetUserId()
+        {
+            if (String.IsNullOrEmpty(CustomUserId))
+                return HttpContext.Current.User.Identity.GetUserId();
+            else
+                return CustomUserId;
+        }
+
+        public void SetUserId(string userId)
+        {
+            this.CustomUserId = userId;
+        }
+    }
+}
---
+++ C#/UserIdGetter/UserIdGetterMock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Services.UserIdGetter
+{
+    public class UserIdGetterMock : IUserIdGetter
+    {
+        public string GetUserId()
+        {
+            return "134b3602-23e2-4459-ab95-1e1d64592db7s";
+        }
+
+        public void SetUserId(string userId)
+        {
+            return;
+        }
+    }
+}

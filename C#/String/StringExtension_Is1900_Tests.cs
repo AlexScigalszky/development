106a376d200839cece9fbd5d2cc947852b30f649
Alex P. Scigalszky <alexpablo.90@gmail.com>
2022-06-14 19:04:20 -0300

    Create StringExtension_Is1900_Tests.cs

---
+++ C#/String/StringExtension_Is1900_Tests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace Trafer.Core.Utils.Tests
+{
+    public class StringExtension_Is1900_Tests
+    {
+        private string _input = string.Empty;
+        private bool _result;
+
+        [Theory]
+        [InlineData("00/01/1900", true)]
+        public void IsValidDateTest(string value, bool result)
+        {
+            GivenAValidInput(value);
+            WhenIs1900IsCalled();
+            ThenReturn(result);
+        }
+
+        private void ThenReturn(bool v)
+        {
+            Assert.Equal(v, _result);
+        }
+
+        private void WhenIs1900IsCalled()
+        {
+            _result = _input.Is1900();
+        }
+
+        private void GivenAValidInput(string value)
+        {
+            _input = value;
+        }
+    }
+}
